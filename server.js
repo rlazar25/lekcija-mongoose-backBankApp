fda71abf8606fb243833c2c794d32563e84d5282
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-06-16 21:08:54 +0200

    Created add, delete and update routes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('./database/config');
 const app = express();
 
 app.use(express.json());
+app.use('/api/v1', require('./routes/index'));
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
